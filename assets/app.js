501f36466af7032f79a5da7f3eecc9a4bc18aecf
Jacob Fenwick <jacobfenwick111@gmail.com>
2019-08-28 21:12:43 -0400

    hidding api keys and importing to app.js file

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    console.log(ticketAPI);
+    console.log(bandsAPI);
+});
+
+// var ticketAPI = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&keyword=movements&apikey=EUnqG6jlJ5Of975tN9AYBMOjFs1wpPt5"
+// var bandsAPI = "https://rest.bandsintown.com/artists/movements?app_id=06a0bf01f7da2f97b38deff3c911b9cd"
